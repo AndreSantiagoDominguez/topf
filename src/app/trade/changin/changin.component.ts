ff86d02d0b5c95b36f4df856e9ad817e9b86a8e1
AndreSantiagoDominguez <233393@ids.upchiapas.edu.mx>
2024-12-06 15:52:12 -0600

    consumiendo servicios

---
+++ src/app/trade/changin/changin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-changin',
+  templateUrl: './changin.component.html',
+  styleUrl: './changin.component.css'
+})
+export class ChanginComponent {
+
+}
---
+++ src/app/trade/trade.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChanginComponent } from './changin/changin.component';
+
+
+
+@NgModule({
+  declarations: [
+    ChanginComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TradeModule { }
